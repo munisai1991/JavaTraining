524afb710097ab7cc2a21e546e1938323d038e79
CG4 <CG4@192.168.1.64>
2016-03-15 23:17:20 +0530

    modified

--- src/com/cloudgensys/training/OrderOfN.java
+++ src/com/cloudgensys/training/OrderOfN.java
@@ -4,10 +4,10 @@ public class OrderOfN {
 	public static void main(String[] args) {
 		float start = System.nanoTime();
 		for(int i=0;i<=100;i++){
-			System.out.println(": "+i);
+			System.out.println("Iteration: "+i);
 		}
 		float total = System.nanoTime() - start;
-		System.out.println("Total Execution time: "+total);
+		System.out.println(" "+total);
 	}
 
 }
