4f4cf9fcbc39acbfefa07281aa96eda6bae25ac8
Sairaj Munigala <sairaj.munigala@cloudgensys.com>
2017-09-20 15:52:54 +0530

    Update Test.java

--- src/com/cloudgensys/training/Test.java
+++ src/com/cloudgensys/training/Test.java
@@ -35,7 +35,7 @@ public class Test {
 		    Matcher matcher2 = contactpattern.matcher(totalString.toString());
 		    if(matcher2.find())
 		    System.out.println("Contact:"+matcher2.group(1));
-		    else System.out.println("contact number is in incorrect format");
+		    else System.out.println("contact number is in incorrect format than required");
 		}
 		catch(Exception e)
 		{
